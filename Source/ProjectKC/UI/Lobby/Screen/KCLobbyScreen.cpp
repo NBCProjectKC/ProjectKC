66d5c549a8bc5d1b85aa109ba02c77f9f98d70a3
dltmdwo2323@gmail.com <sky_battle@naver.com>
2026-08-24 05:21:49 +0900

    메인 사이클 UI WBP/Class 생성
    - HUD
    - Loading
    - Lobby
    - MainMenu
    - PauseMenu
    - Result 화면
    - Settings 화면

---
+++ Source/ProjectKC/UI/Lobby/Screen/KCLobbyScreen.cpp
@@ -0,0 +1,1 @@
+#include "ProjectKC/UI/Lobby/Screen/KCLobbyScreen.h"
---
+++ Source/ProjectKC/UI/Settings/Screen/KCSettingsScreen.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ProjectKC/UI/Common/Widget/KCActivatableWidget.h"
+#include "KCSettingsScreen.generated.h"
+
+UCLASS(Abstract, Blueprintable)
+class PROJECTKC_API UKCSettingsScreen : public UKCActivatableWidget
+{
+	GENERATED_BODY()
+};
