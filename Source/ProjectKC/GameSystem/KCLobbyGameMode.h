ddb781a5a36857af20898c95941ba497bd58c93f
fanccin <77671419+fanccin@users.noreply.github.com>
2026-08-24 16:04:02 +0900

    KCLobbyGameMode 생성 로비 레벨 전용 GameMode. - 인원이 다 찼는지 체크 - 다 찼으면 전투 레벨로 이동

---
+++ Source/ProjectKC/GameSystem/KCLobbyGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "KCLobbyGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTKC_API AKCLobbyGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
