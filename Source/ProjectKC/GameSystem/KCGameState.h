787f3a816372b4264262ecd2a7c06e79fbb4307e
fanccin <77671419+fanccin@users.noreply.github.com>
2026-08-26 20:53:30 +0900

    레벨 관리용 Enum/Library 추가 - EKCLevelType 추가 (6개 레벨: SplashScreen/MainMenu/LoadInLevel/LobbyLevel/Loading/GasRange) - KCLevelTypeLibrary 추가: Enum ↔ 실제 레벨 이름 변환 담당 (레벨명 하드코딩 방지)

--- Source/ProjectKC/GameSystem/KCGameState.h
+++ Source/ProjectKC/GameSystem/KCGameState.h
@@ -3,7 +3,7 @@
 #include "CoreMinimal.h"
 #include "GameFramework/GameState.h"
 #include "GameplayTagContainer.h"
-#include "KCGamePhaseType.h"
+#include "GameSystem/KCGamePhaseType.h"
 #include "KCGameState.generated.h"
 
 /**
