52144033d09076a9a2fc9c67e59ec2187cc1926e
dltmdwo2323@gmail.com <sky_battle@naver.com>
2026-09-09 04:21:43 +0900

    Loading C++ 코드로 전환

--- Source/ProjectKC/UI/Loading/Screen/KCLoadingScreen.h
+++ Source/ProjectKC/UI/Loading/Screen/KCLoadingScreen.h
@@ -4,8 +4,35 @@
 #include "ProjectKC/UI/Common/Widget/KCUserWidget.h"
 #include "KCLoadingScreen.generated.h"
 
+class UProgressBar;
+class UTextBlock;
+class UKCLoadingViewModel;
+
 UCLASS(Abstract, Blueprintable)
 class PROJECTKC_API UKCLoadingScreen : public UKCUserWidget
 {
 	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "KC|Loading")
+	void SetLoadingViewModel(UKCLoadingViewModel* InLoadingViewModel);
+
+	UFUNCTION(BlueprintCallable, Category = "KC|Loading")
+	void RefreshFromViewModel();
+
+protected:
+	virtual void NativeConstruct() override;
+
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidgetOptional), Category = "KC|Loading")
+	TObjectPtr<UProgressBar> ProgressBar_286;
+
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidgetOptional), Category = "KC|Loading")
+	TObjectPtr<UTextBlock> Tip_Text;
+
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidgetOptional), Category = "KC|Loading")
+	TObjectPtr<UTextBlock> TextBlock_72;
+
+private:
+	UPROPERTY(Transient)
+	TObjectPtr<UKCLoadingViewModel> LoadingViewModel;
 };
